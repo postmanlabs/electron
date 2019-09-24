a35245b627eaa077ffaf5ba3633cd5e822fa3d61
Harendra Singh <66hari@gmail.com>
2019-09-24 14:33:06 +0530

    Adds 'call' in front of all commands

--- .buildkite/windows/build.bat
+++ .buildkite/windows/build.bat
@@ -2,19 +2,19 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM (@TODO check do we need to cleanup everything or just the dist & out directories)
 echo "Cleaning up"
-npm run clean
+CALL npm run clean
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Running bootstrap command"
-python script\bootstrap.py -v
+CALL python script\bootstrap.py -v
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Building electron in debug mode"
-python script\build.py -c D
+CALL python script\build.py -c D
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Uploading artifacts"
-call buildkite-agent artifact upload "out/D/electron.exe"
+CALL buildkite-agent artifact upload "out/D/electron.exe"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 exit /b
