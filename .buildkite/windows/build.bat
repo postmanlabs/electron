c80cda98714f4576bf76a41103b89fba93e29d30
Harendra Singh <66hari@gmail.com>
2019-09-24 15:34:15 +0530

    Run the tests after building electron

--- .buildkite/windows/build.bat
+++ .buildkite/windows/build.bat
@@ -13,8 +13,16 @@ echo "Building electron in debug mode"
 CALL python script\build.py -c D
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo "Uploading artifacts"
-CALL buildkite-agent artifact upload "out/D/electron.exe"
+echo "Uploading the artifacts"
+CALL buildkite-agent artifact upload "out/D"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "Running test suite"
+CALL python script\test.py --ci --rebuild_native_modules
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "Running verify ffmpeg"
+CALL python script\verify-ffmpeg.py
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 exit /b
