034865cf3f1887be7186461907e359aa8b541664
Harendra Singh <66hari@gmail.com>
2019-09-25 13:56:36 +0530

    Testing the upload step

--- .buildkite/windows/build-and-upload.bat
+++ .buildkite/windows/build-and-upload.bat
@@ -1,17 +1,17 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM (@TODO check do we need to cleanup everything or just the dist & out directories)
-echo "Cleaning up"
-CALL npm run clean
-if %errorlevel% neq 0 exit /b %errorlevel%
+REM echo "Cleaning up"
+REM CALL npm run clean
+REM if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo "Running bootstrap command"
-CALL python script\bootstrap.py -v
-if %errorlevel% neq 0 exit /b %errorlevel%
+REM echo "Running bootstrap command"
+REM CALL python script\bootstrap.py -v
+REM if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo "Building electron in debug mode"
-CALL python script\build.py -c D
-if %errorlevel% neq 0 exit /b %errorlevel%
+REM echo "Building electron in debug mode"
+REM CALL python script\build.py -c D
+REM if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Uploading the artifacts"
 CALL buildkite-agent artifact upload "out/D/**/*"
