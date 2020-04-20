2d3eb725c90a56993208a53242e74220a813139d
Harendra Singh <66hari@gmail.com>
2020-04-20 19:56:27 +0530

    Updated the scripts to publish to npm manually

--- npm/install.js
+++ npm/install.js
@@ -34,7 +34,10 @@ downloadArtifact({
   force: process.env.force_no_cache === 'true',
   cacheRoot: process.env.electron_config_cache,
   platform: process.env.npm_config_platform || process.platform,
-  arch: process.env.npm_config_arch || process.arch
+  arch: process.env.npm_config_arch || process.arch,
+  mirrorOptions: {
+    mirror: 'https://github.com/postmanlabs/electron/releases/download/'
+  }
 }).then((zipPath) => extractFile(zipPath)).catch((err) => onerror(err))
 
 // unzips and makes path.txt point at the correct executable
