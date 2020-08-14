63a538cde3d1fd212f9155cd72c40da7cf0ba1fc
Harshil Jain <harshil.jain@postman.com>
2020-08-14 10:24:41 +0530

    Increased time

---
+++ .buildkite/build-and-test-pipeline.js
@@ -0,0 +1,58 @@
+function waitStep() {
+  return {
+    wait: true
+  }
+}
+
+function buildStepForWindows () {
+  return {
+    label: ':windows: :electron: Build',
+    timeout_in_minutes: 150,
+    command: '.\\src\\buildkite-upload-script\\electron\\.buildkite\\windows\\build-and-test',
+    agents: [
+      'os=windows',
+      'queue=electron-build-v7'
+    ]
+  };
+}
+
+/**
+ * Returns the build step for linux or darwin
+ * @param {String} platform can be 'linux' or 'darwin'
+ */
+function buildStepForNix (platform) {
+  if(!process.env.BUILDKITE_BRANCH){
+    return [];
+  }
+
+  return {
+    label: `:${platform}: :electron: Build`,
+    timeout_in_minutes: 150,
+    command: [`.buildkite/nix/build-and-test.sh ${platform}`],
+    agents: [
+      `os=${platform}`,
+      'queue=electron-build-v7.2'
+    ]
+  };
+}
+
+function generateBuildPipeline () {
+  return [
+    buildStepForWindows(),
+    buildStepForNix('linux'),
+    buildStepForNix('darwin'),
+  ];
+}
+
+function startBuildPipeline () {
+  const pipeline = generateBuildPipeline();
+
+  console.log(JSON.stringify(pipeline, null, 4));
+}
+
+module.exports = {
+  generateBuildPipeline,
+  startBuildPipeline
+};
+
+!module.parent && startBuildPipeline();
