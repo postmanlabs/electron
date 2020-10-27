eb2eb9759d7fc981d7a9cf3de608e3899f821f54
Harshil Jain <harshil.jain@postman.com>
2020-10-27 17:04:19 +0530

    Added newer pipeline files

---
+++ .buildkite/electron-test-pipeline.js
@@ -0,0 +1,58 @@
+function waitStep() {
+  return {
+    wait: true
+  }
+}
+
+function buildStepForWindows () {
+  return {
+    label: ':windows: :electron: Build',
+    timeout_in_minutes: 300,
+    command: '.\\src\\buildkite-upload-script\\electron\\.buildkite\\windows\\electron-test',
+    agents: [
+      'os=windows',
+      'queue=electron-build-v9'
+    ]
+  };
+}
+
+/**
+ * Returns the build step for linux or darwin
+ * @param {String} platform can be 'linux' or 'darwin'
+ */
+function buildStepForNix (platform) {
+  if(!process.env.BUILDKITE_BRANCH){
+    return [];
+  }
+
+  return {
+    label: `:${platform}: :electron: Build`,
+    timeout_in_minutes: 300,
+    command: [`.buildkite/nix/electron-test.sh ${platform}`],
+    agents: [
+      `os=${platform}`,
+      'queue=electron-build-v9'
+    ]
+  };
+}
+
+function generateBuildPipeline () {
+  return [
+    // buildStepForWindows(),
+    buildStepForNix('linux'),
+    // buildStepForNix('darwin'),
+  ];
+}
+
+function startBuildPipeline () {
+  const pipeline = generateBuildPipeline();
+
+  console.log(JSON.stringify(pipeline, null, 4));
+}
+
+module.exports = {
+  generateBuildPipeline,
+  startBuildPipeline
+};
+
+!module.parent && startBuildPipeline();
