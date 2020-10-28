de3c85ea57cd47ad4df60dc6a24697469779e5c7
Harshil Jain <harshil.jain@postman.com>
2020-10-28 13:11:42 +0530

    Added back pipeline steps

--- .buildkite/windows/electron-test.bat
+++ .buildkite/windows/electron-test.bat
@@ -46,7 +46,7 @@ CALL set SCCACHE_PATH=%cd%\electron\external_binaries\sccache.exe
 ECHO "--- Building electron binaries in Release mode for 32 bit"
 CALL gn gen out/Testing --args="import(\"//electron/build/args/release.gn\") cc_wrapper=\"%SCCACHE_PATH%\"" || EXIT /b !errorlevel!
 
-CALL ninja -C out/Testing electron:electron_app -j 50 || EXIT /b !errorlevel!
+CALL ninja -C out/Testing electron:electron_app -j 10 || EXIT /b !errorlevel!
 CALL ninja -C out/Testing third_party\electron_node:headers || EXIT /b !errorlevel!
 
 CALL cd out/Testing || EXIT /b !errorlevel!
